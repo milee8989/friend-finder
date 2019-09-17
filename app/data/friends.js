edd8b508a7bdf44509bc620a56f13ed58c1903da
milee8989 <milee8989@gmail.com>
2019-09-17 15:34:55 -0500

    everyting work but sumib button

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+var friendsArray = [
+    {
+      friendName: "Panda",
+      friendImg: "https://www.nationalgeographic.com/content/dam/ngdotcom/rights-exempt/animals-search/search-light-left.adapt.676.1.jpg",
+      scores: [
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
+      ]
+    },
+    {
+      friendName: "Tiger",
+      friendImg: "https://www.toledozoo.org/wp-content/uploads/2018/01/IMG_0082cc.jpg",
+      scores: [
+          3,
+          1,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1,
+          3,
+          1
+      ]
+    },
+    {
+        friendName: "Zebra",
+      friendImg: "https://www.stlzoo.org/files/6114/7749/6039/reproductive.jpg",
+      scores: [
+          5,
+          5,
+          5,
+          5,
+          4,
+          4,
+          4,
+          4,
+          1,
+          1
+      ]
+    },
+  {
+    friendName: "lamb",
+    friendImg:"https://aldf.org/wp-content/uploads/2018/05/lamb-iStock-665494268-16x9-e1559777676675.jpg",
+  scores: [
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+  ]
+}
+];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
